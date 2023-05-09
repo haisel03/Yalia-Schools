31ccbce2b7b248dd1b94bc29acd16445295b997e
Haisel Ramirez <haiselramirez@hotmail.com>
2023-05-08 22:12:48 -0400

    Commit Inicial

---
+++ YaliaSchools/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace YaliaSchools.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
